17517cb265ed8df39e205bb208b6f1dd129217e8
Jonatan Santos Barrios <jonatan.santos.88@gmail.com>
2015-07-23 12:16:26 +0200

    Sequelize y SQLite3

--- controllers/quiz-controller.js
+++ controllers/quiz-controller.js
@@ -1,15 +1,19 @@
+var models = require('../models/models.js');
+
+// GET /quizes/question
 exports.question = function(req, res) {
-	res.render('quizes/question', {pregunta: 'Capital de Italia'});
+	models.Quiz.findAll().success(function(quiz) {
+		res.render('quizes/question', {pregunta: quiz[0].pregunta});
+	})
 };
 
+// GET /quizes/answer
 exports.answer = function(req, res) {
-	if(req.query.respuesta === 'Roma'){
-		console.log("Correcto");
-		console.log(req.query.respuesta);
-		res.render('quizes/answer', {respuesta: "Correcto"});
-	} else {
-		console.log("Incorrecto");
-		console.log(req.query.respuesta);
-		res.render('quizes/answer', {respuesta: "Incorrecto"});
-	}
+	models.Quiz.findAll().success(function(quiz){
+		if(req.query.respuesta === quiz[0].respuesta){
+			res.render('quizes/answer', {respuesta: "Correcto"});
+		} else {
+			res.render('quizes/answer', {respuesta: "Incorrecto"});
+		}
+	})
 }
---
+++ models/models.js
@@ -0,0 +1,27 @@
+var path = require('path');
+
+// Cargar Modelo ORM
+var Sequelize = require('sequelize');
+
+// Usar BBDD SQLite
+var sequelize = new Sequelize(null, null, null,
+					{dialect: "sqlite", storage: "quiz.sqlite"});
+
+// Importar la definicion de la tabla Quiz en quiz.js
+var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+
+exports.Quiz = Quiz; // Exportar definición de tabla Quiz
+
+// sequelize.sync() crea el manejador de una vez creada la tabla
+sequelize.sync().success(function() {
+	// success(..) ejecuta el manejador una vez creada la tabla
+	Quiz.count().success(function(){
+		if(count === 0) {
+			Quiz.create({ pregunta: 'Capital de Italia',
+						  respuesta: 'Roma'
+						})
+			.success(function(){console.log('Base de datos inicializada')});
+		};
+	});
+});
+
