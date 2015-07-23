84134f87b3a064d974a7fbc29be6f5bcd54064fc
Jonatan Santos Barrios <jonatan.santos.88@gmail.com>
2015-07-23 13:26:01 +0200

    Despliegue DB en Heroku

--- models/models.js
+++ models/models.js
@@ -1,11 +1,29 @@
 var path = require('path');
 
+// Postgres DATABASE_URL = postgres://user:password@host:port/database
+// SQLite DATABASE_URL = sqlite://:@/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6]||null);
+var user = (url[2]||null);
+var pwd = (url[3]||null)
+var protocol = (url[1]||null);
+var dialect = (url[1]||null);
+var port = (url[5]||null);
+var host = (url[4]||null);
+var storage = process.env.DATABASE_STORAGE;
+
 // Cargar Modelo ORM
 var Sequelize = require('sequelize');
 
 // Usar BBDD SQLite
-var sequelize = new Sequelize(null, null, null,
-					{dialect: "sqlite", storage: "quiz.sqlite"});
+var sequelize = new Sequelize(DB_name, user, pwd,
+					{ dialect: protocol,
+					  protocol: protocol,
+					  port: port,
+					  host: host,
+					  storage: storage,
+					  omitNull: true
+					});
 
 // Importar la definicion de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
@@ -13,19 +31,15 @@ var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 exports.Quiz = Quiz; // Exportar definición de tabla Quiz
 
 // sequelize.sync() crea el manejador de una vez creada la tabla
-sequelize.sync().success(function() {
+sequelize.sync().then(function() {
 	// success(..) ejecuta el manejador una vez creada la tabla
-	Quiz.count().success(function(){
+	Quiz.count().then(function(){
 		if(count === 0) {
 			Quiz.create({ pregunta: 'Capital de Italia',
 						  respuesta: 'Roma'
 						})
-			.success(function(){console.log('Base de datos inicializada')});
+			.then(function(){console.log('Base de datos inicializada')});
 		};
 	});
-<<<<<<< HEAD
-});
-=======
 });
 
->>>>>>> origin/master
