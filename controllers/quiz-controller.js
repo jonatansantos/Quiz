05fa78c5e43cebc04defed711d297e804cd5f492
Jonatan Santos Barrios <jonatan.santos.88@gmail.com>
2015-07-31 18:01:34 +0200

    Añadir temática a preguntas

--- controllers/quiz-controller.js
+++ controllers/quiz-controller.js
@@ -50,7 +50,7 @@ exports.answer = function(req, res) {
 // GET /quizes/new
 exports.new = function( req, res) {
 	var quiz = models.Quiz.build(
-		{pregunta: "Pregunta", respuesta: "Respuesta"}
+		{pregunta: "Pregunta", respuesta: "Respuesta", tematica: 'Temática'}
 	);
 
 	res.render('quizes/new', {quiz: quiz, errors: []});
@@ -76,7 +76,7 @@ exports.create = function(req, res) {
 			} else {
 				// Guarda en DB los campos pregunta y respuesta de quiz
 				quiz
-				.save({fields: ["pregunta", "respuesta"]})
+				.save({fields: ["pregunta", "respuesta", "tematica"]})
 				.then(function(){
 					res.redirect('/quizes');
 				});
@@ -89,6 +89,7 @@ exports.create = function(req, res) {
 exports.update = function(req, res) {
 	req.quiz.pregunta = req.body.quiz.pregunta;
 	req.quiz.respuesta = req.body.quiz.respuesta;
+	req.quiz.tematica = req.body.quiz.tematica;
 
 	req.quiz
 	.validate()
@@ -98,7 +99,7 @@ exports.update = function(req, res) {
 				res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
 			} else {
 				req.quiz
-				.save( {fields: ["pregunta", "respuesta"]})
+				.save( {fields: ["pregunta", "respuesta", "tematica"]})
 				.then( function(){ res.redirect('/quizes'); });
 			}
 		}
--- models/models.js
+++ models/models.js
@@ -36,10 +36,12 @@ sequelize.sync().then(function() {
 	Quiz.count().then(function(count){
 		if(count === 0) {
 			Quiz.create({ pregunta: 'Capital de Italia',
-						  respuesta: 'Roma'
+						  respuesta: 'Roma',
+						  tematica: 'Otro'
 						});
 			Quiz.create({ pregunta: 'Capital de Portugal',
-						  respuesta: 'Lisboa'
+						  respuesta: 'Lisboa',
+						  tematica: 'Otro'
 						})
 			.then(function(){console.log('Base de datos inicializada')});
 		};
